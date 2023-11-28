9ddb3b8cee52dee82a791a8f0f8a7308b10b098a
jyWooooo <wjy970423@gmail.com>
2023-11-28 11:22:15 +0900

    Add PlayerName Bounding Box, TileMap Composite Collider
    캐릭터 이름을 감싸는 검은 박스 추가
    타일맵 콜라이더에 Composite Collider2D 추가

--- Assets/Scripts/PlayerNameText.cs
+++ Assets/Scripts/PlayerNameText.cs
@@ -6,9 +6,17 @@ using TMPro;
 public class PlayerNameText : MonoBehaviour
 {
     [SerializeField] TextMeshPro nameText;
+    [SerializeField] Transform boundingBox;
+
+    public Vector2 boundingBoxMagin = new(0.1f, 0.1f);
 
     void Start()
     {
         nameText.text = DataManager.Instance.PlayerName;
+        InvokeRepeating(nameof(SetBoundingBox), 0.1f, 1f);
+    }
+    void SetBoundingBox()
+    {
+        boundingBox.localScale = (Vector2)nameText.bounds.size + boundingBoxMagin;
     }
 }
